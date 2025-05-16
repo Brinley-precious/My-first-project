2f9cb3e904325eed28d38191782af56e5d6760cc
Brinley Precious <imukprecious@gmail.com>
2025-05-16 13:04:55 +0100

    Completed the team design project

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class Journal
+{
+    public List<Entry> _entries;
+
+    public Journal()
+    {
+        _entries = new List<Entry>();
+    }
+
+    public void AddEntry(Entry newEntry)
+    {
+        // Placeholder for adding an entry
+    }
+
+    public void DisplayAll()
+    {
+        // Placeholder for displaying all entries
+    }
+
+    public void SaveToFile(string file)
+    {
+        // Placeholder for saving to file
+    }
+
+    public void LoadFromFile(string file)
+    {
+        // Placeholder for loading from file
+    }
+}
---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class PromptGenerator
+{
+    public List<string> _prompts;
+
+    public PromptGenerator()
+    {
+        _prompts = new List<string>();
+    }
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+}
