f6abf3e609de30912c803bb2b56a25568119e454
Brinley Precious <imukprecious@gmail.com>
2025-06-07 01:24:15 +0100

    Completed the learning activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Create a base Assignment
+        Assignment generalAssignment = new Assignment("Brinley Francis", "World History");
+        Console.WriteLine(generalAssignment.GetSummary());
+        Console.WriteLine();
+
+        // Create a MathAssignment
+        MathAssignment mathAssignment = new MathAssignment("Brinley Francis", "Algebra", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+
+        // Create a WritingAssignment
+        WritingAssignment writingAssignment = new WritingAssignment("Brinley Francis", "English Literature", "Exploring Symbolism in Modern Poetry");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
